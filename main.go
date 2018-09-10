fbaaa6c33d15aba589803eecd6545d4c4d10d6af
Yunier Perez <yperez@twilio.com>
2018-09-10 09:42:57 -0500

    Add http endpoint to control the drone

--- main.go
+++ main.go
@@ -4,8 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/felixge/ardrone/client"
+	"github.com/go-chi/chi"
+	"log"
+	"net/http"
 	"os"
 	"time"
+)
+
+const (
+	responseTemplate = `<Response><Say language="en-US">%s</Say><Gather timeout="10" numDigits="1" maxSpeechTime="2" language="en-US" input="speech dtmf" hints="take off, land" finishOnKey=""/><Redirect method="POST">/dron</Redirect></Response>`
+	rotationSpeed    = 0.3
+	movSpeed         = 0.1
+)
+
+type Command struct {
+	duration time.Duration
+	state    ardrone.State
+}
+
+var (
+	dtmfCommands = map[string]Command{
+		"#": {duration: 3 * time.Second, state: ardrone.State{Fly: true}},                      // Take off
+		"*": {duration: 1 * time.Second, state: ardrone.State{Fly: false}},                     // Land
+		"1": {duration: 2 * time.Second, state: ardrone.State{Fly: true, Yaw: -rotationSpeed}}, // Counterclockwise
+		"3": {duration: 2 * time.Second, state: ardrone.State{Fly: true, Yaw: rotationSpeed}},  // Clockwise
+		"2": {duration: 2 * time.Second, state: ardrone.State{Fly: true, Pitch: movSpeed}},     // Forward
+	}
 )
 
 func main() {
@@ -13,6 +37,35 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Drone connected")
+
+	srv := &http.Server{Addr: ":8080"}
+	r := chi.NewRouter()
+	r.Post("/dron", func(w http.ResponseWriter, r *http.Request) {
+		feedback := "What are my orders?"
+
+		r.ParseForm()
+		d := r.Form.Get("Digits")
+		fmt.Println(d)
+		if d != "" {
+			if c, ok := dtmfCommands[d]; ok {
+				client.ApplyFor(c.duration, c.state)
+				feedback = "Done"
+			} else {
+				feedback = "Unknown Command"
+			}
+
+		}
+		w.Header().Set("Content-type", "text/xml")
+		w.Write([]byte(fmt.Sprintf(responseTemplate, feedback)))
+	})
+	http.Handle("/", r)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			// cannot panic, because this probably is an intentional close
+			log.Printf("Httpserver: ListenAndServe() error: %s", err)
+		}
+	}()
 
 	scanner := bufio.NewScanner(os.Stdin)
 	var t string
@@ -23,44 +76,16 @@ func main() {
 		if err := scanner.Err(); err != nil {
 			fmt.Println("Error reading from input: ", err)
 		}
-
-		// scanner.Text() strips new lines
-		// so in case of just a new line
-		// it's actually an empty string
 		if t == "q" {
 			client.Land()
+			if err := srv.Shutdown(nil); err != nil {
+				panic(err)
+			}
 			break
 		}
 
-		if t == "#" {
-			client.Takeoff()
-			time.Sleep(3 * time.Second)
-		}
-
-		if t == "*" {
-			client.Land()
-		}
-
-		if t == "1" {
-			client.Counterclockwise(0.3)
-			time.Sleep(1 * time.Second)
-			client.Hover()
-			time.Sleep(1 * time.Second)
-		}
-
-		if t == "3" {
-			client.Clockwise(0.3)
-			time.Sleep(1 * time.Second)
-			client.Hover()
-			time.Sleep(1 * time.Second)
-		}
-
-		if t == "2" {
-			client.Forward(0.4)
-			time.Sleep(1 * time.Second)
-			client.Hover()
-			time.Sleep(1 * time.Second)
+		if c, ok := dtmfCommands[t]; ok {
+			client.ApplyFor(c.duration, c.state)
 		}
 	}
-	client.Land()
 }
